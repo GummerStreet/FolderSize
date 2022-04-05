21cb6020b470641b1fa644f94066e7fb6ff45f34
GummerStreet <92685677+GummerStreet@users.noreply.github.com>
2022-04-05 13:58:28 +0200

    Add files via upload

---
+++ AddContextMenu.py
@@ -0,0 +1,16 @@
+import os
+
+regfile=open("AddContextMenu.reg",'w')
+regfile.write("Windows Registry Editor Version 5.00"+"\n\n"+"[HKEY_CLASSES_ROOT\Directory\shell\FolderSize]"+"\n")
+path= os.getcwd()
+pathlength=len(path)
+newpath=""
+for n in range (0,pathlength):
+    newpath=(newpath+path[n])
+    if path[n]=="\\":
+        newpath=newpath+"\\"
+regfile.write('"icon"'+"="+'"'+newpath+'\\'+'\\'+"folder32.ico"+'"'+"\n\n")
+regfile.write("[HKEY_CLASSES_ROOT\Directory\shell\FolderSize\command]"+"\n")
+regfile.write('@="'+ newpath+'\\'+'\\'"foldersize.exe"+" "+'\\'+'"'+"%1"+"\\"+'""')
+regfile.close()
+os.system("reg import AddContextMenu.reg")
