a65f1a2b127784f005e669d6d96b0d7a3d9f9bca
GummerStreet <92685677+GummerStreet@users.noreply.github.com>
2022-04-08 10:56:47 +0200

    Add files via upload

--- foldersize.py
+++ foldersize.py
@@ -6,36 +6,63 @@ from tkinter.constants import *
 from tkinter import *
 import io
 
-img=b'iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAABmJLR0QA/wD/AP+gvaeTAAAACXBIWXMAAC4jAAAuIwF4pT92AAAAB3RJTUUH5gMcFDI18UpbNgAAABl0RVh0Q29tbWVudABDcmVhdGVkIHdpdGggR0lNUFeBDhcAAAGHSURBVDjLnZPLSwJRFMZ/d+6kBmUPqjHsIWGCQg96QJCLaBH9C/0f/U9tolXbSshFywJR0p70spKgGmrUmbktpAnLNPp25557Ps53zneEUkrRAKpcAdOqBR0BhN/X6Bv6j8KLe9x0DrLF+kQihJaMIyJG3bPwOrAdnN1DSBVoiqUYcnkKdFnfwf7eJuK5gN6vMf849jtBKo8DyJWZLwL7skjPyB19syGymWtOtx4a6xWSsL8bPZVHxcKIiFEjcNM5mKspGYsauOsN54ptOxxsn5K8jeGmc8iIga7KFVSuCHNQOL7DsmykFL9LmIQN64C17AKqXEH3VgVUqw4TU8NNZ+i6irNMCZ4A00L7TJQeTQaMLlrBNC0G74NerNERAODqskRff2dLgtcXi4GnoGcwTfh9iLgBgj/h4rxENGBAIoTw+2oSZDJBeKi3ZbFtu7yevBNsa0dLxr98IEdDaNkRTnZumhJYb1UWb6KwGvMs/c3KR5DKt7DyOHJ52rOy+H6N/z+mf57zB3hioi9FsuHVAAAAAElFTkSuQmCC'
-root= tk.Tk()
-top= root
-top.geometry("600x450+468+138")
-top.title("Folder Size")
-favicon=tk.PhotoImage(data=img) 
-root.wm_iconphoto(True, favicon)
+#create main window
+def create_main_window():
+        global top
+        global root
+        img=b'iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAABmJLR0QA/wD/AP+gvaeTAAAACXBIWXMAAC4jAAAuIwF4pT92AAAAB3RJTUUH5gQIBzQ2TSKs+wAAA1FJREFUWMPVl0tvE1cYhp8znhqHCcYmdi52cILjVihAWkIEBTVSlAUXBcGaHXtWSCyR2PQXFKmLLrokYgviIlWVqipCqKFQhYhFwYE4sSF33yY29tiHhR0nE9tNEw8Czm7OzJn3+b7vXN4jpJSST9jU7Q6QRgHiOizGzS88LnBpCNX2EQCkRIZjyIlp5JPIf34qBgKIvi5Ejw+E2PLXYqsSyNfvKN57CrHk9lLlc6KM9CMOtO8QwChQfPg38tFUQzUWp4IoZ49BndLUBJB6hsLoGGJqyZKJJoMt2C79gNCatgZIpVL89cst0hOlyN27NI65u3HY7I1RBFuwXR6uyoQJwDAMbv50g8FhJ44mexkoy583X3DNP9JwJsSpIMr546Y+ZePD0qs3eNrTdAZa8Hj34PHu4UDQi3dorzWleDSFfP2uzjKUsjTbO6oH2u0qd6PPEDsQPd7SQ6vDWXku3nuK7cq5yhKtAMhwDKLxmgAnB78mM5DbtrhhFBi9/5hLb79fh4glkeEYIuTfBDAxXfMn2WweoQh2a7t2lPaTQyH++Xma0x1H2KhlApBGoeYON/54igd3JmBHyS+1fN7AvmBjMjXLd64uhlt7kU8iyAsnEKqtnIG4XjUw/HKOeFzn+o8XURTR8AScfB7lj99ecDTbhduulTQ9zvIq2HywlKMfHDpoiThALl8gkzHQ1HIpy5pKvQG6nsPh+Moa8ZyBnn5Pd9SNXVHr7wNrbXZmmd7DPsvO/GQiS3xZp1fzV72rCTAzvcj+wD7LAFZWdBbnU4ScbXUAPC5T55vwMu0+l2UAiUSG+XCCzqZ9ZgNTAXBplf50MotQJOo2nU29trqaQ0+/51BqU0nLmgqAUG2IgQAAz8YjhL5psy76+CrLSzqH93aaXVM5wMocEH1dALydSdIdbLVuAiazRCNLhJrXg1rTMgP0+MDvomO/E4+32RLxYlGysrIKsZKvWLNqosdX4zQUAmWkn6PNv1sWfTqdJZnI0JdfX37KSL/JrJp2hd3+Nhbm0iwupCwBePnvHJNjEa56z1QMyWaTWmXJotFZxn+9jXg13zBAs+rgW3egtPf/H0u20ZQWR8fAUlM6iNAcX4gt/zwuJp/L1exjX07Fp76efwCi6XAcvCTwRQAAAABJRU5ErkJggg=='
+        root= tk.Tk()
+        top= root
+        top.geometry("600x450+468+138")
+        top.title("Folder Size")
+        favicon=tk.PhotoImage(data=img) 
+        root.wm_iconphoto(True, favicon)
 
 #Textbox
-textbox = Text(top)
-textbox.place(relx=0.033, rely=0.022, relheight=0.878, relwidth=0.933)
-scroll_1=Scrollbar (top)
-scroll_1.pack(side=RIGHT, fill=Y)
-textbox.configure(yscrollcommand=scroll_1.set)
-scroll_1.configure(command=textbox.yview)
+def create_textbox():
+        global textbox
+        textbox = Text(top)
+        textbox.place(relx=0.033, rely=0.022, relheight=0.878, relwidth=0.933)
+        scroll_1=Scrollbar (top)
+        scroll_1.pack(side=RIGHT, fill=Y)
+        textbox.configure(yscrollcommand=scroll_1.set)
+        scroll_1.configure(command=textbox.yview)
 
-def copy_text():
-        textbox.event_generate(("<<Copy>>"))
+#menu
+def create_menu():
+    menubar=tk.Menu(top, tearoff=0)
+    top.configure(menu=menubar)
+    sub_menu=tk.Menu(top, tearoff=0)
+    menubar.add_cascade(menu=sub_menu,compound="left", label="Edit")
+    sub_menu.add_command(compound="left",label="Copy", command=copy_code)
+    sub_menu.add_command(compound="left",label="Quit", command=QuitApp)
+
+#Quit
+def QuitApp():
+    top.destroy()
+
+#Copy Code
+def copy_code():
+    textbox.tag_add(SEL, "1.0", END)
+    textbox.event_generate(("<<Copy>>"))
 
 #CopyContextMenu
-menu = Menu(root, tearoff = 0)
-menu.add_command(label="Copy", command=copy_text)
+def create_context_menu():
+        global menu
+        menu = Menu(root, tearoff = 0)
+        menu.add_command(label="Copy", command=copy_text)
+        root.bind("<Button-3>", context_menu)
 
 def context_menu(event): 
     try: 
         menu.tk_popup(event.x_root, event.y_root)
     finally: 
         menu.grab_release()
+        
+def copy_text():
+        textbox.event_generate(("<<Copy>>"))
 
-base_dir=str(sys.argv[1])
+
 def dir_size(path):
     total_size = 0
 
@@ -46,29 +73,41 @@ def dir_size(path):
             
             total_size += os.path.getsize(f)
     return total_size
-dirtext=(dir_size(base_dir))
-formatteddirsize=str(format(dirtext,","))
-textbox.insert(INSERT, formatteddirsize+"\n")
-    
-files = os.listdir(base_dir)
 
-folders = []
+def list_folder_size():
+        global base_dir
+        base_dir=str(sys.argv[1])
+        dirtext=(dir_size(base_dir))
+        formatteddirsize=str(format(dirtext,","))
+        textbox.insert(INSERT,"folder path: "+base_dir+"\n")
+        textbox.insert(INSERT, formatteddirsize+"\n")
+            
+        files = os.listdir(base_dir)
 
-for e in files:
-    filepath = os.path.join(base_dir, e)
-    if os.path.isdir(filepath):
-        folders.append({"name":e, "path":filepath, "size":dir_size(filepath)})
+        folders = []
 
-folders.sort(key=lambda filename: filename['size'], reverse=True)
-for f in folders:
-    textbox.insert(INSERT, ("#"*50)+"\n")
-    textbox.insert(INSERT, ("folder name: " + f['name'])+"\n")
-    textbox.insert(INSERT, ("folder path: " + f['path'])+"\n")
-    foldersize=f['size']
-    formattedsize=format(foldersize,",")
-    textbox.insert(INSERT, ("folder size: " + formattedsize)+"\n")
-textbox.configure(state=DISABLED)
+        for e in files:
+            filepath = os.path.join(base_dir, e)
+            if os.path.isdir(filepath):
+                folders.append({"name":e, "path":filepath, "size":dir_size(filepath)})
+
+        folders.sort(key=lambda filename: filename['size'], reverse=True)
+        for f in folders:
+            textbox.insert(INSERT, ("#"*50)+"\n")
+            textbox.insert(INSERT, ("folder name: " + f['name'])+"\n")
+            textbox.insert(INSERT, ("folder path: " + f['path'])+"\n")
+            foldersize=f['size']
+            formattedsize=format(foldersize,",")
+            textbox.insert(INSERT, ("folder size: " + formattedsize)+"\n")
+        textbox.configure(state=DISABLED)
 
 
-root.bind("<Button-3>", context_menu)
+def main():
+        create_main_window()
+        create_textbox()
+        create_menu()
+        create_context_menu()
+        list_folder_size()
+        
+main()
 root.mainloop()
